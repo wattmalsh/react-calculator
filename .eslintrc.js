6e2aceae95b05525ae5992478e272c45fa732a2d
Matt Walsh <walsh.matt.t@gmail.com>
2017-12-10 20:39:24 -0800

    Update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  "extends": "airbnb",
-  "parserOptions": {
-    "ecmaScript": 6,
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaScript: 6,
   },
-}
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  },
+  env: {
+    browser: true,
+  },
+};
